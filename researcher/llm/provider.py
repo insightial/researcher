de73339c3475ba4e7cf04d0bdf31efee1e8d9a65
regokan <regokan@gmail.com>
2024-10-24 16:44:12 +0530

    feat: add memory to LLM Provider

--- researcher/llm/provider.py
+++ researcher/llm/provider.py
@@ -1,5 +1,8 @@
-from langchain_openai import ChatOpenAI
 from typing import Any
+
+from langchain.chains import ConversationChain
+from langchain_community.chat_models import ChatOpenAI
+from langchain_core.memory import BaseMemory
 
 
 class LLMProvider:
@@ -7,22 +10,29 @@ class LLMProvider:
     LLM provider
     """
 
-    def __init__(self, llm):
+    def __init__(self, llm, memory: BaseMemory = None):
         self.llm = llm
+        self.memory = memory
+        if memory:
+            self.chain = ConversationChain(llm=llm, memory=memory, verbose=True)
 
     @classmethod
-    def create_provider(cls, provider: str, **kwargs: Any):
+    def create_provider(cls, provider: str, memory: BaseMemory = None, **kwargs: Any):
         """
         Create an LLM provider
         """
         if provider == "openai":
-            return cls(ChatOpenAI(**kwargs))
-        else:
-            raise ValueError(f"Unknown LLM provider: {provider}")
+            llm = ChatOpenAI(**kwargs)
+            return cls(llm, memory)
+        raise ValueError(f"Unknown LLM provider: {provider}")
 
     async def agenerate(self, prompt: str) -> str:
         """
         Generate a response asynchronously
         """
-        response = await self.llm.agenerate([prompt])
-        return response.generations[0][0].text
+        if self.memory:
+            response = await self.chain.arun(prompt)
+        else:
+            response = await self.llm.agenerate([prompt])
+            response = response.generations[0][0].text
+        return response
