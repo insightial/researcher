64ff95e06b67b5c6141aaad005693732eb2a280a
regokan <regokan@gmail.com>
2024-10-29 13:52:45 +0530

    fix: handle double $ sign rendering

--- client/components/message.py
+++ client/components/message.py
@@ -1,6 +1,5 @@
 import streamlit as st
 
-
 def message(content, is_user=False):
     """
     Display a chat message in the Streamlit app.
@@ -8,9 +7,11 @@ def message(content, is_user=False):
     Args:
     content (str): The message content to display.
     is_user (bool): True if the message is from the user, False if from the assistant.
-    key (str): An optional unique key for the message component.
     """
+    # Escape dollar signs to prevent LaTeX rendering in Streamlit
+    safe_content = content.replace("$", "\$")
+    
     if is_user:
-        st.chat_message("user").write(content)
+        st.chat_message("user").write(safe_content)
     else:
-        st.chat_message("assistant").write(content)
+        st.chat_message("assistant").write(safe_content)
