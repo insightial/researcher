9ec0d659932fb99e466696945a496bbdeaaa1c34
regokan <regokan@gmail.com>
2024-10-30 12:47:15 +0530

    refactor: update toast from thread dialog

--- client/dialog/thread.py
+++ client/dialog/thread.py
@@ -4,27 +4,25 @@ from hooks import update_thread_name, delete_thread
 
 
 @st.dialog("Change thread name")
-def update_thread_name_dialog(cookies, thread_id, current_name, message_container):
+def update_thread_name_dialog(cookies, thread_id, current_name):
     new_name = st.text_input("Enter new thread name:", value=current_name)
     if st.button("Update Name"):
         update_thread_name(cookies, thread_id, new_name)
-        with message_container:
-            st.toast("Thread name updated")
-            time.sleep(3)
-            st.empty()
+        st.toast("Thread name updated")
+        time.sleep(1)
+        st.empty()
         st.rerun()
 
 
 @st.dialog("Delete Thread")
-def delete_thread_dialog(cookies, thread_id, message_comtainer):
+def delete_thread_dialog(cookies, thread_id):
     st.subheader("Are you sure you want to delete?")
     if st.button("Delete"):
         status_code = delete_thread(cookies, thread_id)
-        with message_comtainer:
-            if status_code == 200:
-                st.toast("Thread deleted")
-            else:
-                st.toast("Error deleting the thread")
-            time.sleep(3)
-            st.empty()
+        if status_code == 200:
+            st.toast("Thread deleted")
+        else:
+            st.toast("Error deleting the thread")
+        time.sleep(1)
+        st.empty()
         st.rerun()
